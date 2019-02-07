2a9924112d85c0074c1729da888762d309ee2597
Michael Zhang <mzhang00@stuy.edu>
2019-02-07 09:13:51 -0500

    Update toString method

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,4 +3,11 @@ public class QueenBoard{
   public QueenBoard(int size){
     board = new int[size][size];
   }
+  public String toString(){
+    for (int i = 0; i < board.length; i++){
+      for (int c = 0; c < board.length; i++){
+        return "";
+      }
+    }
+  }
 }

a56b6370e683c7fd550796ddb2ed85e9053ec8fd
Michael Zhang <mzhang00@stuy.edu>
2019-02-08 09:45:25 -0500

    Test and fix bugs relating to toString

--- QueenBoard.java
+++ QueenBoard.java
@@ -2,11 +2,16 @@ public class QueenBoard{
   private int[][] board;
   public QueenBoard(int size){
     board = new int[size][size];
+    for (int i = 0; i < board.length; i++){
+      for (int c = 0; c < board.length; c++){
+        board[i][c] = 0;
+      }
+    }
   }
   public String toString(){
     String ans = "";
     for (int i = 0; i < board.length; i++){
-      for (int c = 0; c < board.length; i++){
+      for (int c = 0; c < board.length; c++){
         if (board[i][c] != -1){
           ans += "_";
         }else{
